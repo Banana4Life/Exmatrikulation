4a67ff5d4f4db64ce6b44c3772b6407f4fedb2e2
JonasDann <jonas.chr.dann@gmail.com>
2014-09-17 19:32:23 +0200

    The image is now a decal and rotating :D Yay

--- core/src/de/cubeisland/games/dhbw/DHBWGame.java
+++ core/src/de/cubeisland/games/dhbw/DHBWGame.java
@@ -2,30 +2,45 @@ package de.cubeisland.games.dhbw;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Quaternion;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
+import com.badlogic.gdx.graphics.g3d.decals.Decal;
+import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
 
 public class DHBWGame extends ApplicationAdapter {
-	SpriteBatch batch;
+	DecalBatch batch;
 	Texture img;
+    PerspectiveCamera camera;
+    Decal testDecal;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+        camera = new PerspectiveCamera(45, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.near = 1;
+        camera.far = 300;
+        camera.position.set(0, 0, 5);
+
+		batch = new DecalBatch(new CameraGroupStrategy(camera));
+
 		img = new Texture("badlogic.jpg");
+        testDecal = Decal.newDecal(1, 1, new TextureRegion(img));
+        testDecal.setPosition(0, 0, -1);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setTransformMatrix(new Matrix4(new Vector3(0, 0, 0), new Quaternion(new Vector3(0, 0, 1), 30), new Vector3(1, 1, 1)));
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        camera.update();
+
+        testDecal.rotateX(0.5f);
+
+        batch.add(testDecal);
+		batch.flush();
 	}
 }
