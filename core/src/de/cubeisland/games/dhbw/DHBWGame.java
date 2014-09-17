b021f469b1e276d0c07eaab647085716d2f2c1a0
Jonas Dann <jonas.chr.dann@gmail.com>
2014-09-17 12:01:58 +0200

    I dont know if the transformation matrix works #yolo

--- core/src/de/cubeisland/games/dhbw/DHBWGame.java
+++ core/src/de/cubeisland/games/dhbw/DHBWGame.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
 
 public class DHBWGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -20,6 +23,7 @@ public class DHBWGame extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setTransformMatrix(new Matrix4(new Vector3(0, 0, 0), new Quaternion(new Vector3(0, 0, 1), 30), new Vector3(1, 1, 1)));
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
