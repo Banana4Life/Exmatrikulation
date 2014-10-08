e840b0d3d238d8cb1e2fa5c06ea4b42463d6ea99
Phillip Schichtel <phillip.public@schich.tel>
2014-10-08 19:37:35 +0200

    added a D20 model

--- core/src/de/cubeisland/games/dhbw/Dice.java
+++ core/src/de/cubeisland/games/dhbw/Dice.java
@@ -1,12 +1,15 @@
 package de.cubeisland.games.dhbw;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.TextureProvider;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 
@@ -19,10 +22,13 @@ public class Dice {
     public Dice(Board board) {
         this.board = board;
 
-        ModelBuilder modelBuilder = new ModelBuilder();
-        Model model = modelBuilder.createBox(5f, 5f, 5f,
-                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        ObjLoader ldr = new ObjLoader();
+        Model model = ldr.loadModel(Gdx.files.internal("D20.obj"));
+
+//        ModelBuilder modelBuilder = new ModelBuilder();
+//        Model model = modelBuilder.createBox(5f, 5f, 5f,
+//                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+//                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         instance = new ModelInstance(model);
         instance.transform.setToTranslation(0, 0, -50);
         speed = new Vector3(10f, 0, 0);
