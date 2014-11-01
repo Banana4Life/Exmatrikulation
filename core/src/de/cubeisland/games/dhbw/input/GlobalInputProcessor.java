82553b83fd8863c4f67c6b68b4e9760e1ac38250
Phillip Schichtel <phillip.public@schich.tel>
2014-11-01 12:58:21 +0100

    drop old and broken code

--- core/src/de/cubeisland/games/dhbw/input/GlobalInputProcessor.java
+++ core/src/de/cubeisland/games/dhbw/input/GlobalInputProcessor.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.utils.Array;
 import de.cubeisland.games.dhbw.entity.component.Pickable;
 import de.cubeisland.games.dhbw.entity.component.Picked;
 import de.cubeisland.games.dhbw.entity.component.Render;
@@ -23,32 +24,6 @@ public class GlobalInputProcessor implements InputProcessor {
         this.engine = engine;
         this.sm = stateManager;
     }
-
-//    @Override
-//    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-//        if (button == Input.Buttons.LEFT) {
-//            ImmutableArray<Entity> entities = this.engine.getEntitiesFor(Family.getFor(Render.class));
-//
-//            for (int i = 0; i < entities.size(); ++i) {
-//                Entity e = entities.get(i);
-//                float x = screenX;
-//                float y = Gdx.graphics.getHeight() - screenY;
-//                if (e.getComponent(Render.class).getObject().isWithin(camera, x, y)) {
-//                    state().entityTouched(e, x, y);
-//                }
-//            }
-//            ImmutableArray<Entity> entities = engine.getEntitiesFor(Family.getFor(Pickable.class, Render.class));
-//            for (int i = 0; i < entities.size(); ++i) {
-//                Entity entity = entities.get(i);
-//                if (entity.getComponent(Render.class).getObject().isWithin(camera, screenX, Gdx.graphics.getHeight() - screenY)) {
-//                    entity.remove(Pickable.class);
-//                    entity.add(new Picked());
-//                }
-//            }
-//        }
-//        return true;
-//    }
-
 
     @Override
     public boolean keyDown(int keycode) {
@@ -72,13 +47,7 @@ public class GlobalInputProcessor implements InputProcessor {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        if (button == Input.Buttons.LEFT) {
-            for (Entity entity : engine.getEntitiesFor(Family.getFor(Picked.class))) {
-                entity.remove(Picked.class);
-                entity.add(new Pickable());
-            }
-        }
-        return true;
+        return false;
     }
 
     @Override
