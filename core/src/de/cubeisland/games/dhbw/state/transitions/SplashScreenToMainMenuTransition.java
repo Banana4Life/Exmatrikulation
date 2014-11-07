cc49ff86beefd6ed7d266f8fd853230d9a054399
Tim Adamek <tim.adamek@web.de>
2014-11-08 00:34:02 +0100

    Cards don't overlap with deck anymore

--- core/src/de/cubeisland/games/dhbw/state/transitions/SplashScreenToMainMenuTransition.java
+++ core/src/de/cubeisland/games/dhbw/state/transitions/SplashScreenToMainMenuTransition.java
@@ -30,7 +30,7 @@ public class SplashScreenToMainMenuTransition extends StateTransition {
         DHBWGame game = context.getGame();
 
         Entity deck = game.getEntityFactory().create(game.getResources().entities.deck);
-        deck.getComponent(Transform.class).setPosition(new Vector3(40, 0, -150)).setRotation(new Quaternion(new Vector3(0, 1, 0), -90));
+        deck.getComponent(Transform.class).setPosition(new Vector3(60, 0, -150)).setRotation(new Quaternion(new Vector3(0, 1, 0), -90));
         deck.getComponent(Deck.class).setDestPos(new Vector3(0, 0, -150)).setDestRot(new Quaternion(new Vector3(1, 0, 0), 0));
 
         game.getEngine().addEntity(deck);
