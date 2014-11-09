b4874efd077da964b2985c058399e9a1bdf411e7
Jonas Dann <jonas.chr.dann@gmail.com>
2014-11-09 21:42:42 +0100

    Fixed a problem with clicking on the event card in the reacting state

--- core/src/de/cubeisland/games/dhbw/state/states/ReactingState.java
+++ core/src/de/cubeisland/games/dhbw/state/states/ReactingState.java
@@ -40,16 +40,18 @@ public class ReactingState extends GameState {
     public boolean touchDown(StateContext context, int screenX, int screenY, int pointer, int button) {
         Entity entity = EntityUtil.getEntityAt(context.getEngine(), context.getCamera(), screenX, screenY);
 
-        if (entity != null && button == Input.Buttons.LEFT) {
-            Entity cardHand = context.getEngine().getEntitiesFor(Family.one(CardHand.class).get()).first();
-            if (entity.getComponent(Card.class) != null && cardHand.getComponent(CardHand.class).highlightCard(entity)) {
-                cardHand.getComponent(CardHand.class).playCard();
-                //TODO apply card to character
-                return true;
-            } else {
-                entity.getComponent(Dice.class).setTicks(60);
-                context.transitionTo(DecidingState.ID);
-                return true;
+        if (entity != null) {
+            if (button == Input.Buttons.LEFT) {
+                Entity cardHand = context.getEngine().getEntitiesFor(Family.one(CardHand.class).get()).first();
+                if (entity.getComponent(Card.class) != null && cardHand.getComponent(CardHand.class).highlightCard(entity)) {
+                    cardHand.getComponent(CardHand.class).playCard();
+                    //TODO apply card to character
+                    return true;
+                } else if (entity.getComponent(Dice.class) != null) {
+                    entity.getComponent(Dice.class).setTicks(60);
+                    context.transitionTo(DecidingState.ID);
+                    return true;
+                }
             }
         }
 
